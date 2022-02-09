f1398efca103552435829bfce71cd3fdc04eb54c
dagrimb <74441727+dagrimb@users.noreply.github.com>
2022-02-08 17:13:21 -0800

    import and inject MAT_DIALOG_DATA in details component

--- src/app/movie-details/movie-details.component.ts
+++ src/app/movie-details/movie-details.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { FetchApiDataService } from '../fetch-api-data.service';
+import {MAT_DIALOG_DATA} from '@angular/material/dialog';
 
 @Component({
   selector: 'app-movie-details',
@@ -6,8 +8,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./movie-details.component.scss']
 })
 export class MovieDetailsComponent implements OnInit {
-
-  constructor() { }
+  movie: any = {};
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: {title: string, description: string, source: string,
+     year: number, score: string},
+    public fetchApiData: FetchApiDataService
+    ) {}
 
   ngOnInit(): void {
   }
