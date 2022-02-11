934b340db34e28fe7157dd7dc39c5da43c95788f
dagrimb <74441727+dagrimb@users.noreply.github.com>
2022-02-11 14:46:58 -0800

    display favorites in user profile and create functionality to nav back to homepage

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -4,6 +4,10 @@ import { Router } from '@angular/router';
 import { MatDialog } from '@angular/material/dialog';
 import { EditProfileComponent } from '../edit-profile/edit-profile.component';
 import { DeleteProfileComponent } from '../delete-profile/delete-profile.component';
+import { MovieDetailsComponent } from '../movie-details/movie-details.component';
+import { MovieDirectorComponent } from '../movie-director/movie-director.component';
+import { MovieGenreComponent } from '../movie-genre/movie-genre.component';
+
 
 @Component({
   selector: 'app-user-profile',
@@ -13,7 +17,8 @@ import { DeleteProfileComponent } from '../delete-profile/delete-profile.compone
 
 export class UserProfileComponent implements OnInit {
   user: any = localStorage.getItem('user');  // delcare user variable us object where user data will be stored
-  
+  favorites: any = [];
+
   constructor(
     public fetchApiData: FetchApiDataService,
     public router: Router,
@@ -22,6 +27,7 @@ export class UserProfileComponent implements OnInit {
 
   ngOnInit(): void {
     this.getThisUser(); // function to fetch user data from FetchApiDatService
+    this.getUserFavorites();
   }
 
   getThisUser(): void {
@@ -46,6 +52,54 @@ openDeleteProfileDialog(): void {
     width: '600px'
   });
 }
+
+
+getUserFavorites(): void {
+  let faves: any [] = [];
+  this.fetchApiData.getAllMovies().subscribe((response: any) => {
+    faves = response;
+    faves.forEach((fave: any) => {
+      if (this.user.FavoriteMovies.includes(fave._id)) {
+        this.favorites.push(fave);
+      }
+    });
+  });
+  console.log(this.favorites);
+  return this.favorites;
+}
+
+
+
+openMovieGenreDialog(image: any, name: string, description: string, source: string): void {
+  this.dialog.open(MovieGenreComponent, {
+    data: { image, name, description, source},
+    height: '400px',
+    width: '600px'
+  });
+}
+
+openMovieDirectorDialog(image: any, name: string, birth: number, death: number, bio: string,
+source: string): void {
+  this.dialog.open(MovieDirectorComponent, {
+    data: { image, name, birth, death, bio, source},
+    height: '400px',
+    width: '600px'
+  });
+}
+
+openMovieSynopsisDialog(title: string, description: any, source: string,
+  year: number, score: string): void {
+  this.dialog.open(MovieDetailsComponent, {
+    data: { title, description, source, year, score},
+    height: '400px',
+    width: '600px',
+  });
+}
+
+backHome(): void {
+  this.router.navigate(['movies'])
+}
+
 
 
 }
